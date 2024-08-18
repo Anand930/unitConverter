c214c39849982998daa804798fa2a81f2d4d2d05
Anand Sharma <anandkumarsharma93068@gmail.com>
2024-08-18 06:10:22 -0700

    color changes and make some text bold

--- src/InputBox.jsx
+++ src/InputBox.jsx
@@ -4,13 +4,13 @@ const InputBox = ({ selectValue, options, inputValue, inputOnChange,selectOnChan
   return (
     <div className="flex items-center justify-center gap-6 mx-auto ">
       <input
-        className="border-2 border-orange-700 w-52 h-12 px-2 bg-orange-300 rounded-xl"
+        className="border-2 border-orange-700 w-52 h-12 px-4 bg-orange-300 text-orange-700 font-bold  rounded-xl"
         value={inputValue}
         type="text" 
         onChange={inputOnChange}
         readOnly={readOnly}
       />
-      <select className="w-40 h-12 border-2 border-orange-700 bg-orange-300 rounded-lg" value={selectValue} onChange={selectOnChange}> 
+      <select className="w-40 h-12 border-2 border-orange-700 text-orange-700 px-3 font-bold bg-orange-300 rounded-lg" value={selectValue} onChange={selectOnChange}> 
         {options}
       </select>
     </div>
